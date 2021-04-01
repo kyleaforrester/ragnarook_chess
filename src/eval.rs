7b0efbaeeea82f714cac6336cc2faa65550205af
Kyle Forrester <kyleaforrester@gmail.com>
2021-03-31 21:25:36 -0400

    Updating HCE

--- src/eval.rs
+++ src/eval.rs
@@ -3,19 +3,32 @@ use crate::misc;
 use crate::move_gen;
 use crate::search::Ending;
 
+// Material
+const P_VAL: u32 = 100;
+const N_VAL: u32 = 300;
+const B_VAL: u32 = 300;
+const R_VAL: u32 = 500;
+const Q_VAL: u32 = 900;
+
+// Mobility
+const n_mob: f32 = 10.0;
+const b_mob: f32 = 10.0;
+const r_mob: f32 = 10.0;
+const q_mob: f32 = 4.0;
+
 pub fn evaluate(board: &board::Board) -> (Option<Ending>, f32) {
     let end = eval_ending(board);
 
-    let w_material = 100 * board.w_p_bb.count_ones()
-        + 300 * board.w_n_bb.count_ones()
-        + 300 * board.w_b_bb.count_ones()
-        + 500 * board.w_r_bb.count_ones()
-        + 900 * board.w_q_bb.count_ones();
-    let b_material = 100 * board.b_p_bb.count_ones()
-        + 300 * board.b_n_bb.count_ones()
-        + 300 * board.b_b_bb.count_ones()
-        + 500 * board.b_r_bb.count_ones()
-        + 900 * board.b_q_bb.count_ones();
+    let w_material = P_VAL * board.w_p_bb.count_ones()
+        + N_VAL * board.w_n_bb.count_ones()
+        + N_VAL * board.w_b_bb.count_ones()
+        + R_VAL * board.w_r_bb.count_ones()
+        + Q_VAL * board.w_q_bb.count_ones();
+    let b_material = P_VAL * board.b_p_bb.count_ones()
+        + N_VAL * board.b_n_bb.count_ones()
+        + N_VAL * board.b_b_bb.count_ones()
+        + R_VAL * board.b_r_bb.count_ones()
+        + Q_VAL * board.b_q_bb.count_ones();
 
     let w_pawn_score = pawn_score(true, board.w_p_bb);
     let w_mobility = mobility(board, true);
@@ -48,7 +61,7 @@ fn pawn_score(is_w: bool, bb: u64) -> f32 {
 
         let rank = if is_w { pos / 8 } else { 7 - (pos / 8) };
 
-        score += (150.0 / 48.0) * (rank.pow(2) as f32) - (150.0 / 48.0);
+        score += (100.0 / 48.0) * (rank.pow(2) as f32) - (100.0 / 48.0);
 
         p_bb = p_bb & (p_bb - 1);
     }
@@ -56,10 +69,6 @@ fn pawn_score(is_w: bool, bb: u64) -> f32 {
 }
 
 fn mobility(board: &board::Board, is_white: bool) -> f32 {
-    let n_mult = 10.0;
-    let b_mult = 10.0;
-    let r_mult = 10.0;
-    let q_mult = 5.0;
     let mut score = 0.0;
 
     if is_white {
@@ -72,7 +81,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.w_n_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += n_mult * (move_gen::solo_knight_moves(bb, allies).count_ones() as f32);
+            score += n_mob * (move_gen::solo_knight_moves(bb, allies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
 
@@ -80,7 +89,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.w_b_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += b_mult
+            score += b_mob
                 * (move_gen::solo_bishop_moves(bb, allies, allies | enemies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
@@ -89,7 +98,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.w_r_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += r_mult
+            score += r_mob
                 * (move_gen::solo_rook_moves(bb, allies, allies | enemies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
@@ -98,7 +107,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.w_q_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += q_mult
+            score += q_mob
                 * ((move_gen::solo_bishop_moves(bb, allies, allies | enemies).count_ones()
                     + move_gen::solo_rook_moves(bb, allies, allies | enemies).count_ones())
                     as f32);
@@ -114,7 +123,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.b_n_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += n_mult * (move_gen::solo_knight_moves(bb, allies).count_ones() as f32);
+            score += n_mob * (move_gen::solo_knight_moves(bb, allies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
 
@@ -122,7 +131,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.b_b_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += b_mult
+            score += b_mob
                 * (move_gen::solo_bishop_moves(bb, allies, allies | enemies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
@@ -131,7 +140,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.b_r_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += r_mult
+            score += r_mob
                 * (move_gen::solo_rook_moves(bb, allies, allies | enemies).count_ones() as f32);
             bb = bb & (bb - 1);
         }
@@ -140,7 +149,7 @@ fn mobility(board: &board::Board, is_white: bool) -> f32 {
         let mut bb = board.b_q_bb;
         while bb > 0 {
             let lsb = bb & (!bb + 1);
-            score += q_mult
+            score += q_mob
                 * ((move_gen::solo_bishop_moves(bb, allies, allies | enemies).count_ones()
                     + move_gen::solo_rook_moves(bb, allies, allies | enemies).count_ones())
                     as f32);
